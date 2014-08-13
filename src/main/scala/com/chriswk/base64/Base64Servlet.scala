2ff33bb8007f84fccc37524be93bfff6eb4a033f
Christopher Kolstad <christopher.kolstad@gmail.com>
2014-08-13 07:40:46 +0200

    Added md5 encoder

--- src/main/scala/com/chriswk/base64/Base64Servlet.scala
+++ src/main/scala/com/chriswk/base64/Base64Servlet.scala
@@ -2,7 +2,8 @@ package com.chriswk.base64
 
 import org.scalatra._
 import scalate.ScalateSupport
-import java.util.Base64;
+import java.util.Base64
+import java.security.MessageDigest
 
 class Base64Servlet extends Base64funStack {
 
@@ -21,6 +22,10 @@ class Base64Servlet extends Base64funStack {
 
   get("/decode") {
     new String(Base64.getDecoder().decode(params("q")))
+  }
+  
+  get("/md5") {
+	  MessageDigest.getInstance("MD5").digest(params("q").getBytes()).map("%02X".format(_)).mkString
   }
   
 }
